050de7b1b0cf97a4dc820b8563925f4dad19aecc
weeryan17 <theweeryan17@gmail.com>
2016-09-16 11:26:06 -0500

    Start on ww.
    Fixing local repo.

---
+++ Simple ww/src/com/weeryan17/sww/util/mannagers/PlayerMannager.java
@@ -0,0 +1,12 @@
+package com.weeryan17.sww.util.mannagers;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class PlayerMannager {
+	
+	public ArrayList<Player> getWerewolves(){
+		return null;
+	}
+}
---
+++ Simple ww/src/com/weeryan17/sww/util/mannagers/WerewolfMannager.java
@@ -0,0 +1,7 @@
+package com.weeryan17.sww.util.mannagers;
+
+public class WerewolfMannager {
+	public static PlayerMannager getPlayerMannager(){
+		return new PlayerMannager();
+	}
+}
