852cabc5bc0270a26561eb346cfbe5e03d55f744
weeryan17 <theweeryan17@gmail.com>
2017-02-28 12:41:38 -0600

    Organized logging.

--- Discord Groups/Discord Groups - Server/src/com/weeryan17/dgs/util/Logging.java
+++ Discord Groups/Discord Groups - Server/src/com/weeryan17/dgs/util/Logging.java
@@ -46,17 +46,92 @@ public class Logging {
 	
 	public void log(String message, boolean discord){
 		if(discord){
-			try {
-				logChannel.sendMessage(message);
-			} catch (MissingPermissionsException e) {
-				this.log("Discord bot is missing permisions", DiscordLevels.DiscordMissingPerms, e, false);
-			} catch (RateLimitException e) {
-				this.log("Discord bot hit it's rate limit", DiscordLevels.DiscordRateLimit, e, false);
-			} catch (DiscordException e) {
-				this.log("Discord bot ran into a error", DiscordLevels.Discord, e, false);
-			}	
+			sendMessage(message);
+		}
+
+		Date dateobj = new Date();
+		
+		DateFormat secoondsf = new SimpleDateFormat("HH:mm:ss:SSS");
+		String secondsDate = secoondsf.format(dateobj);
+		
+		System.out.println(secondsDate + " [DiscordGroups] " + message);
+	}
+	
+	/**
+	 * Logs stuff with a log level. Both to console and file.
+	 * Optionally logs to discord.
+	 * 
+	 * @param message The message to log.
+	 * @param level The level at witch to log it.
+	 * @param discord If you want to log to discord.
+	 */
+	public void log(String message, Level level, boolean discord){
+		if(discord){
+			sendMessage("[" + level.getName() + "] " + message);
 		}
 		
+		Date dateobj = new Date();
+		
+		DateFormat secoondsf = new SimpleDateFormat("HH:mm:ss:SSS");
+		String secondsDate = secoondsf.format(dateobj);
+		
+		writeToFile("[" + level.getName() + "] " + message);
+		
+		logger.log(level, secondsDate + " [DiscordGroups] " + message);
+	}
+	
+	/**
+	 * Logs stuff with a log level, and a Throwable. Both to console and file.
+	 * Optionally logs to discord.
+	 * 
+	 * @param message The message to log.
+	 * @param level The level at witch to log it.
+	 * @param thrown The exception thrown.
+	 * @param discord If you want to log to discord.
+	 */
+	public void log(String message, Level level, Throwable thrown, boolean discord){
+		if(discord){
+			sendMessage("[" + level.getName() + "] " + message);
+		}
+		
+		Date dateobj = new Date();
+		
+		DateFormat secoondsf = new SimpleDateFormat("HH:mm:ss:SSS");
+		String secondsDate = secoondsf.format(dateobj);
+
+		StringBuilder sb = new StringBuilder();
+			
+		sb.append("[" + secondsDate + "] " + "[" + level.getName() + "] " + message);
+		sb.append('\n');
+		sb.append(thrown.getMessage());
+		sb.append('\n');
+		for(StackTraceElement element: thrown.getStackTrace()){
+			sb.append(element.getClassName() + " class generated an error on line " + element.getLineNumber() + " in the method " + element.getMethodName() + "().");
+			sb.append("\n");
+		}
+		
+		writeToFile(sb.toString());
+		
+		if(instance.hasTray()){
+			instance.getIcon().displayMessage("Discord Groups", message, MessageType.ERROR);
+		}
+		
+		logger.log(level, secondsDate + " [DiscordGroups] " + message, thrown);
+	}
+	
+	public void sendMessage(String message){
+		try {
+			logChannel.sendMessage(message);
+		} catch (MissingPermissionsException e) {
+			this.log("Discord bot is missing permisions", Level.WARNING, e, false);
+		} catch (RateLimitException e) {
+			this.log("Discord bot hit it's rate limit", Level.SEVERE, e, false);
+		} catch (DiscordException e) {
+			this.log("Discord bot ran into a error", Level.WARNING, e, false);
+		}
+	}
+	
+	public void writeToFile(String message){
 		Date dateobj = new Date();
 		
 		DateFormat dayf = new SimpleDateFormat("MM.dd.yy");
@@ -92,135 +167,5 @@ public class Logging {
 			this.log("Ran into a io expresion when writting to file " + file.getPath() + "\n" + 
 					"Softly shutting down to prevent further problems", Level.SEVERE, e, false);
 		}
-		System.out.println(secondsDate + " [DiscordGroups] " + message);
 	}
-	
-	/**
-	 * Logs stuff with a log level. Both to console and file.
-	 * Optionally logs to discord.
-	 * 
-	 * @param message The message to log.
-	 * @param level The level at witch to log it.
-	 * @param discord If you want to log to discord.
-	 */
-	public void log(String message, Level level, boolean discord){
-		if(discord){
-			try {
-				logChannel.sendMessage("[" + level.getName() + "] " + message);
-			} catch (MissingPermissionsException e) {
-				this.log("Discord bot is missing permisions", DiscordLevels.DiscordMissingPerms, e, false);
-			} catch (RateLimitException e) {
-				this.log("Discord bot hit it's rate limit", DiscordLevels.DiscordRateLimit, e, false);
-			} catch (DiscordException e) {
-				this.log("Discord bot ran into a error", DiscordLevels.Discord, e, false);
-			}
-		}
-		
-		Date dateobj = new Date();
-		
-		DateFormat dayf = new SimpleDateFormat("MM.dd.yy");
-		String dayDate = dayf.format(dateobj);
-		
-		DateFormat hourf = new SimpleDateFormat("HH");
-		String hourDate = hourf.format(dateobj) + ".00";
-		
-		DateFormat secoondsf = new SimpleDateFormat("HH:mm:ss:SSS");
-		String secondsDate = secoondsf.format(dateobj);
-		
-		BufferedWriter bw = null;
-		FileWriter fw = null;
-		
-		File file = null;
-		
-		try {
-			File dir = new File(instance.getJarLoc() + "/" + logFolder + "/" + dayDate);
-			dir.mkdirs();
-			file = new File(instance.getJarLoc() + "/" + logFolder + "/" + dayDate + "/" + hourDate + ".log");
-			if(!file.exists()){
-				file.createNewFile();
-			}
-			
-			fw = new FileWriter(file, true);
-			bw = new BufferedWriter(fw);
-			
-			bw.write("[" + secondsDate + "] " + "[" + level.getName() + "] " + message);
-			bw.write('\n');
-			
-			bw.flush();
-		} catch (IOException e) {
-			this.log("Ran into a io expresion when writting to file " + file.getPath() + "\n" + 
-					"Softly shutting down to prevent further problems", Level.SEVERE, e, false);
-		}
-		logger.log(level, secondsDate + " [DiscordGroups] " + message);
-	}
-	
-	/**
-	 * Logs stuff with a log level, and a Throwable. Both to console and file.
-	 * Optionally logs to discord.
-	 * 
-	 * @param message The message to log.
-	 * @param level The level at witch to log it.
-	 * @param thrown The exception thrown.
-	 * @param discord If you want to log to discord.
-	 */
-	public void log(String message, Level level, Throwable thrown, boolean discord){
-		if(discord){
-			try {
-				logChannel.sendMessage("[" + level.getName() + "] " + message);
-			} catch (MissingPermissionsException e) {
-				this.log("Discord bot is missing permisions", DiscordLevels.DiscordMissingPerms, e, false);
-			} catch (RateLimitException e) {
-				this.log("Discord bot hit it's rate limit", DiscordLevels.DiscordRateLimit, e, false);
-			} catch (DiscordException e) {
-				this.log("Discord bot ran into a error", DiscordLevels.Discord, e, false);
-			}
-		}
-		
-		Date dateobj = new Date();
-		
-		DateFormat dayf = new SimpleDateFormat("MM.dd.yy");
-		String dayDate = dayf.format(dateobj);
-		
-		DateFormat hourf = new SimpleDateFormat("HH");
-		String hourDate = hourf.format(dateobj) + ".00";
-		
-		DateFormat secoondsf = new SimpleDateFormat("HH:mm:ss:SSS");
-		String secondsDate = secoondsf.format(dateobj);
-		
-		BufferedWriter bw = null;
-		FileWriter fw = null;
-		
-		File file = null;
-		
-		try {
-			File dir = new File(instance.getJarLoc() + "/" + logFolder + "/" + dayDate);
-			dir.mkdirs();
-			file = new File(instance.getJarLoc() + "/" + logFolder + "/" + dayDate + "/" + hourDate + ".log");
-			if(!file.exists()){
-				file.createNewFile();
-			}
-			
-			fw = new FileWriter(file, true);
-			bw = new BufferedWriter(fw);
-			
-			bw.write("[" + secondsDate + "] " + "[" + level.getName() + "] " + message);
-			bw.write('\n');
-			bw.write(thrown.getMessage());
-			bw.write('\n');
-			for(StackTraceElement element: thrown.getStackTrace()){
-				bw.write(element.getClassName() + " class generated an error on line " + element.getLineNumber() + " in the method " + element.getMethodName() + "().");
-				bw.write("\n");
-			}
-			
-			bw.flush();
-		} catch (IOException e) {
-			this.log("Ran into a io expresion when writting to file " + file.getPath() + "\n" + 
-					"Softly shutting down to prevent further problems", Level.SEVERE, e, false);
-		}
-		if(instance.hasTray()){
-			instance.getIcon().displayMessage("Discord Groups", message, MessageType.ERROR);
-		}
-		logger.log(level, secondsDate + " [DiscordGroups] " + message, thrown);
-	}
-	
 }
