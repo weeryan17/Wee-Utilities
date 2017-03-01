675c3a9cca2b9ad58f37906b27da3fe798a9a3b7
weeryan17 <theweeryan17@gmail.com>
2017-03-01 08:48:31 -0600

    Started work on updater.

--- Discord Groups/Discord Groups - Server/src/com/weeryan17/dgs/listeners/discord/RandomListener.java
+++ Discord Groups/Discord Groups - Server/src/com/weeryan17/dgs/listeners/discord/RandomListener.java
@@ -4,6 +4,9 @@ import com.weeryan17.dgs.DiscordGroups;
 
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.handle.impl.events.guild.role.RoleCreateEvent;
+import sx.blah.discord.handle.impl.events.guild.role.RoleDeleteEvent;
+import sx.blah.discord.handle.obj.IRole;
 
 public class RandomListener {
 	
@@ -15,5 +18,15 @@ public class RandomListener {
 	@EventSubscriber
 	public void onReady(ReadyEvent e){
 		discord.readyInit();
+	}
+	
+	@EventSubscriber
+	public void onRoleCreate(RoleCreateEvent e){
+		IRole role = e.getRole();
+	}
+	
+	@EventSubscriber
+	public void onRoleDelete(RoleDeleteEvent e){
+		IRole role = e.getRole();
 	}
 }
---
+++ Discord Groups/Discord Groups - Server/src/com/weeryan17/dgs/util/Update.java
@@ -0,0 +1,72 @@
+package com.weeryan17.dgs.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+import com.weeryan17.dgs.DiscordGroups;
+
+public class Update {
+	DiscordGroups instance;
+	public Update(DiscordGroups instance){
+		this.instance = instance;
+	}
+	
+	/**
+	 * Updates the bot.
+	 * 
+	 * Borrowed most of this from FlareBot.
+	 */
+	public void update(){
+		File git = new File("Discord-Groups" + File.separator);
+		if(!git.exists() || git.isDirectory()){
+			ProcessBuilder clone = new ProcessBuilder("git", "clone", "https://github.com/weeryan17/Wee-Utilities", git.getAbsolutePath());
+			clone.redirectErrorStream(true);
+			Process p = null;
+			try {
+				p = clone.start();
+			} catch (IOException e) {
+				instance.getLogger().log("Chouldn't update", Level.WARNING, e, true);
+			}
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line;
+            try {
+				if ((line = reader.readLine()) != null) {
+				    sb.append(line + '\n');
+				}
+			} catch (IOException e) {
+				instance.getLogger().log("Can't read clone output", Level.WARNING, e, true);
+			}
+            if (p.exitValue() != 0) {
+            	instance.getLogger().log("Clone exited with a non 0 status\n" + sb.toString(), Level.WARNING, true);
+            }
+		} else {
+			ProcessBuilder pull = new ProcessBuilder("git", "pull");
+			pull.directory(git);
+			Process p = null;
+			try {
+				p = pull.start();
+			} catch (IOException e) {
+				instance.getLogger().log("Chouldn't update", Level.WARNING, e, true);
+			}
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line;
+            try {
+				if ((line = reader.readLine()) != null) {
+				    sb.append(line + '\n');
+				}
+			} catch (IOException e) {
+				instance.getLogger().log("Can't read pull output", Level.WARNING, e, true);
+			}
+            if (p.exitValue() != 0) {
+            	instance.getLogger().log("Pull exited with a non 0 status\n" + sb.toString(), Level.WARNING, true);
+            }
+		}
+	}
+}
