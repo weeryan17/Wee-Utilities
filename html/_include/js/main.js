1d685a18b179c45c26bdd3c5ce5743894f266331
weeryan17 <theweeryan17@gmail.com>
2017-04-29 21:34:35 -0500

    New main page woo!

--- html/_include/js/main.js
+++ html/_include/js/main.js
@@ -1,56 +1,100 @@
-jQuery(document).ready(function(event){
-	var projectsContainer = $('.cd-projects-container'),
-		navigation = $('.cd-primary-nav'),
-		triggerNav = $('.cd-nav-trigger'),
-		logo = $('.cd-logo');
-	
-	triggerNav.on('click', function(){
-		if( triggerNav.hasClass('project-open') ) {
-			//close project
-			projectsContainer.removeClass('project-open').find('.selected').removeClass('selected').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				$(this).children('.cd-project-info').scrollTop(0).removeClass('has-boxshadow');
+/*
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
+(function($) {
+
+	skel
+		.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#page-wrapper'),
+			$banner = $('#banner'),
+			$header = $('#header');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
 			});
-			triggerNav.add(logo).removeClass('project-open');
-		} else {
-			//trigger navigation visibility
-			triggerNav.add(projectsContainer).add(navigation).toggleClass('nav-open');
-		}
+
+		// Mobile?
+			if (skel.vars.mobile)
+				$body.addClass('is-mobile');
+			else
+				skel
+					.on('-medium !medium', function() {
+						$body.removeClass('is-mobile');
+					})
+					.on('+medium', function() {
+						$body.addClass('is-mobile');
+					});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly')
+				.scrolly({
+					speed: 1500,
+					offset: $header.outerHeight()
+				});
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-menu-visible'
+				});
+
+		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight() + 1,
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			}
+
 	});
 
-	projectsContainer.on('click', '.single-project', function(){
-		var selectedProject = $(this);
-		if( projectsContainer.hasClass('nav-open') ) {
-			//close navigation
-			triggerNav.add(projectsContainer).add(navigation).removeClass('nav-open');
-		} else {
-			//open project
-			selectedProject.addClass('selected');
-			projectsContainer.add(triggerNav).add(logo).addClass('project-open');
-		}
-	});
-
-	projectsContainer.on('click', '.cd-scroll', function(){
-		//scroll down when clicking on the .cd-scroll arrow
-		var visibleProjectContent =  projectsContainer.find('.selected').children('.cd-project-info'),
-			windowHeight = $(window).height();
-
-		visibleProjectContent.animate({'scrollTop': windowHeight}, 300); 
-	});
-
-	//add/remove the .has-boxshadow to the project content while scrolling 
-	var scrolling = false;
-	projectsContainer.find('.cd-project-info').on('scroll', function(){
-		if( !scrolling ) {
-		 	(!window.requestAnimationFrame) ? setTimeout(updateProjectContent, 300) : window.requestAnimationFrame(updateProjectContent);
-		 	scrolling = true;
-		}
-	});
-
-	function updateProjectContent() {
-		var visibleProject = projectsContainer.find('.selected').children('.cd-project-info'),
-			scrollTop = visibleProject.scrollTop();
-		( scrollTop > 0 ) ? visibleProject.addClass('has-boxshadow') : visibleProject.removeClass('has-boxshadow');
-		scrolling = false;
-	}
-});
+})(jQuery);
--- html/index.php
+++ html/index.php
@@ -1,137 +1,171 @@
-<!doctype html>
-<html lang="en" class="no-js">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="/_include/pace/pace.min.js"></script>
-	<link href="/_include/pace/themes/flash.css" rel="stylesheet" />
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<script src="_include/pace/pace.min.js"></script>
+		<link href="_include/pace/themes/flash.css" rel="stylesheet" />
+		<title>Discord Groups</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="_include/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing scrollbar" id="style-1" >
 
-	<link href='https://fonts.googleapis.com/css?family=Playfair+Display:700italic,700' rel='stylesheet' type='text/css'>
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
 
-	<link rel="stylesheet" href="_include/css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="_include/css/style.css"> <!-- Resource style -->
-	<script src="_include/js/modernizr.js"></script> <!-- Modernizr -->
-  	
-	<title>Home | Discord Groups</title>
-</head>
-<body>
-	<header>
-		<a href="#0" class="cd-logo"><img src="_include/img/cd-logo.png"></a>
-		
-		<button class="cd-nav-trigger">Menu<span aria-hidden="true" class="cd-icon"></span></button>
-	</header>
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="/">Discord Groups</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="">Home</a></li>
+											<li><a href="/dashboard/">Dashboard</a></li>
+											<li><a href="https://github.com/LeCodeCo/Discord-Groups-Wiki/wiki" target="_blank">Wiki</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
 
-	<nav class="cd-primary-nav">
-		<ul>
-			<li class="cd-label">Navigation</li>
-			<li><a href="">Home</a></li>
-			<li></li>
-			<li><a href="dashboard">Dashboard</a></li>
-			<li class="cd-label">External Links</li>
-			<li><a href="https://twitter.com/DiscordGroups">Twitter</a></li>
-			<li><a href="https://discordapp.com/oauth2/authorize?client_id=280192562583699458&scope=bot&permissions=372505664">Discord Bot Invite</a></li>
-			<li><a href="https://discord.gg/GkxJhFq">Support Discord</a></li>
-			<li><a href="https://github.com/LeCodeCo/Discord-Groups-Wiki/wiki/">Wiki</a></li>
-			<li><a href="https://github.com/weeryan17/Wee-Utilities/tree/master/Discord%20Groups">Source Code</a></li>
-		</ul>
-	</nav> <!-- .cd-primary-nav -->
-	
-	<div class="cd-projects-container">
-		<ul>
-			<li class="single-project">
-				<div class="cd-title">
-					<h2>About</h2>
-				</div> <!-- .cd-title -->
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2>Discord Groups</h2>
+							<p>Link Minecraft and Discord<br />
+							First Discord Role Linking Plugin</p>
+							<ul class="actions">
+							  <li><a href="#" target="_blank" class="button special">Download</a></li>
+						  </ul>
+						</div>
+						<a href="#one" class="more scrolly">More</a>
+					</section>
 
-				<div class="cd-project-info">
-					<button class="cd-scroll">Scroll down</button>
-					
-					<div class="content-wrapper">
-						<p>
-							Discord groups is a plugin created by weeryan17 and its original design was to link Ranks (Groups) on your server with Ranks (Roles) on your Discord server.
-					  </p>
+				<!-- One -->
+					<section id="one" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+							  <h2>About Discord Groups</h2>
+							  <p>Discord Groups is the first plugin to link Discord and Minecraft roles together.<br />
+							  You just need to download the plugin, invite the Bot and follow the easy setup guide!<br />
+								It is easily configured in the Dashboard.<br />
+								</p>
+							</header>
+							<ul class="icons major">
+								<li><span class="icon fa-cloud major style1"><span class="label">Lorem</span></span></li>
+								<li><span class="icon fa-heart-o major style2"><span class="label">Ipsum</span></span></li>
+								<li><span class="icon fa-cogs major style3"><span class="label">Dolor</span></span></li>
+							</ul>
+						</div>
+					</section>
 
-						<p>
-							Let's say you wanted to be able to allow ale players with a rank show that they have a rank on the Discord as well. A fast and efficient way would be through Discord Groups. You download the plugin, install it and the rest can be done via the Dashboard.
-						</p>
-						<p><br>
-					  </p>
-						<p><br>
-					  </p>
-						<p><br>
-					  </p>
-					</div>
-				</div> <!-- .cd-project-info -->
-			</li>
+				<!-- Two -->
+					<section id="two" class="wrapper alt style2">
+						<section class="spotlight">
+							<div class="image"><img src="_include/img/pic01.jpg" alt="" /></div><div class="content">
+							  <h2>Add Rank perks to Discord</h2>
+							  <p>Using Discord Groups, you can create perks for players who have paid ranks so they can eg. Use Music Bots, Access certain channels etc.</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="_include/img/pic02.jpg" alt="" /></div><div class="content">
+							  <h2>Easy Setup</h2>
+							  <p>Discord Groups has been designed from the ground up to be simple and easy to configure. We want server managers to easily be able to setup Discord Groups without causing large downtime.</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="_include/img/pic03.jpg" alt="" /></div><div class="content">
+							  <h2>Active Development</h2>
+							  <p>Discord Groups is constantly being upgraded to add more cool features and other stuff to ensure everything runs smoothly and efficiantly.</p>
+							</div>
+						</section>
+					</section>
 
-			<li class="single-project">
-				<div class="cd-title">
-					<h2>Features</h2>
-				</div> <!-- .cd-title -->
+				<!-- Three -->
+					<section id="three" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+							  <h2>Features</h2>
+							  <p>Discord Groups is constantly being worked on!<br />
+							  More features are being worked on!</p>
+							</header>
+							<ul class="features">
+								<li class="icon fa-hdd-o">
+								  <h3>we host the bot</h3>
+								  <p>Discord Groups was made to be simple. We host the bot so you don't need to worry about that.</p>
+								</li>
+								<li class="icon fa-server">
+								  <h3>Vault Support</h3>
+								  <p>Discord Groups support Vault, so nearly all permissions plugins are compatible. Ranks are automaticly detected.</p>
+								</li>
+								<li class="icon fa-code">
+								  <h3>Open Source</h3>
+								  <p>Discord Groups is Open Source, so you can see how it all works. Just check the Github and be fascinated.</p>
+								</li>
+								<li class="icon fa-cloud">
+								  <h3>Auto Sync</h3>
+								  <p>The plugin automatically syncs Ranks with Roles at a set interval, so you don't need to schedule any commands.</p>
+								</li>
+								<li class="icon fa-heart-o">
+								  <h3>Low Resource Usage</h3>
+								  <p>Discord Groups has been coded efficiantly so the resources used by it is at a minimum. This allows your server to focus on handling players.</p>
+								</li>
+								<li class="icon fa-dashboard">
+								  <h3>Dashboard</h3>
+								  <p>Discord Groups is has a Dashboard for people to link thier Minecraft account with thier Discord Account, and to make configuration easy.</p>
+								</li>
+							</ul>
+						</div>
+					</section>
 
-				<div class="cd-project-info">
-					<button class="cd-scroll">Scroll down</button>
+				<!-- CTA -->
+					<section id="cta" class="wrapper style4">
+						<div class="inner">
+						  <header>
+						    <h2>START THE JOURNEY</h2>
+						    <p>Download Discord Groups today and make your server more enjoyable because there are more cool features!</p>
+						  </header>
+							<ul class="actions vertical">
+							  <li><a href="#" target="_blank" class="button fit special">Download</a></li>
+							  <li><a href="/dashboard/" class="button fit">Dashboard</a></li>
+							</ul>
+						</div>
+					</section>
 
-					<div class="content-wrapper">
-						<p>
-							Discord Groups has many features! The features help make it what Discord Groups is. They are designed with care and much effort has been put into it to bring them to you. Here is a list of them.
-						</p>
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="https://twitter.com/DiscordGroups" target="_blank" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="mailto:discordgroups@gmail.com" target="_blank" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+						  <li>&copy; Discord Groups</li>
+						  <li>Coded with Love!</a></li>
+						</ul>
+					</footer>
 
-						<p>
-							&#8226; Create groups on your Discord server to match the groups on your Minecraft server. This can be useful to show people have a paid rank on the Discord as well as on the Server!
-						</p>
+			</div>
 
-						<p>
-							&#8226; Vault Support for a large range of permission plugin support. Nearly every permission plugin has vault support, and you probably already have vault installed.
-						</p>
-						
-						<p>
-							&#8226; Automatically puts Discord Users in the respective Discord groups. This can be useful to sync people who buys a rank / new staff members etc to the Discord.
-						</p>
-						
-						<p>
-							&#8226; Planed administrative features for your Discord.
-						</p>
-						
-						<p>
-							&#8226; Website for linking Minecraft and Discord accounts. You simply type your Discord name and your Minecraft name.
-						</p>
-						
-						<p>
-							&#8226; Not very resource intensive. This is important especially for larger networks.
-						</p>
-					</div>
-				</div> <!-- .cd-project-info -->
-			</li>
+		<!-- Scripts -->
+			<script src="_include/js/jquery.min.js"></script>
+			<script src="_include/js/jquery.scrollex.min.js"></script>
+			<script src="_include/js/jquery.scrolly.min.js"></script>
+			<script src="_include/js/skel.min.js"></script>
+			<script src="_include/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="_include/js/main.js"></script>
 
-			<li class="single-project">
-				<div class="cd-title">
-					<h2 href="dashboard/">Dashboard</h2>
-				</div> <!-- .cd-title -->
-
-				<div class="cd-project-info">
-					<button class="cd-scroll">Scroll down</button>
-					
-					<div class="content-wrapper">
-						<p>
-							The dashboard allows people to link thier Minecraft account with thier Discord account.
-						</p>
-						
-						<p>
-							<a href="dashboard/">Click here to go to the Dashboard</a>
-						<p><br>
-					  </p>
-						<p><br>
-					  </p>
-						<p><br>
-					  </p>
-						</p>
-					</div>
-				</div> <!-- .cd-project-info -->
-			</li>
-		</ul>
-	</div> <!-- .cd-projects-container -->
-<script src="_include/js/jquery-2.1.4.js"></script>
-<script src="_include/js/main.js"></script> <!-- Resource jQuery -->
-</body>
+	</body>
 </html>
